3dce80d66e6fc3ca5bf1dd2b453e3e2629ea5712
Josh Chorlton <jchorlton@gmail.com>
2016-03-25 19:34:30 +0000

    changed date formatting

--- client/src/script/notification.js
+++ client/src/script/notification.js
@@ -5,13 +5,14 @@ class Notification extends React.Component {
 		let imgContainerStyle = {
 			backgroundImage: 'url(' + this.props.data.iconUrl + ')'
 		}
+		let date = new Date(this.props.data.date);
 		return (
 			<a className="notification" href={this.props.data.url} target="_blank">
 				<div style={imgContainerStyle} className="notification-img-container"></div>
 				<div className="notification-text-container">
 					<div className="line1-container">
 						<div className="line1">{this.props.data.line1}</div>
-						<div className="date">{this.props.data.date}</div>
+						<div className="date">{date.toLocaleDateString()}</div>
 					</div>
 					<div className="line2">{this.props.data.line2}</div>
 					<div className="line3">{this.props.data.line3}</div>
